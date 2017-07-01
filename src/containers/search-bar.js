9dcac64d0dc5e467f00b3f8587149e9799bdcc61
Seton Brown <setonb@gmail.com>
2017-07-01 13:15:54 -0400

    Introducing binding context of this from class to a method

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = { term: '' }
+
+      // Binds "this" from the class "onInputChange" to the method called this.onInputChange 
+      this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input type="text" />
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange}
+          placeholder="Get a five-dat forecast in your favorite cities"
+          className="form-control"
+          type="text"
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Search</button>
         </span>
