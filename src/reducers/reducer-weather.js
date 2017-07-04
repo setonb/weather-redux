a2ece41d87f47359179b1cc54fd9ca8858668bed
Seton Brown <setonb@gmail.com>
2017-07-04 12:29:54 -0400

    Wiring up the reducer to not mutate state, but return a new copy with the updated data using the spread operator.

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,3 +1,14 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch(action.type) {
+      case FETCH_WEATHER:
+      // important to not manipulate or mutate state
+      // So, instead of state.push([action.payload.data])
+      // We use state.concat([action.payload.data])
+      // which is identical to the es6 spread form below.
+        return [ action.payload.data, ...state ];
+  }
+
   return state;
 }
